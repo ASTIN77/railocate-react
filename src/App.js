c31b5a860778ab74728c9f6d8e74200c96ea53a9
Andrew Tinsley <atinsley@astech-it.co.uk>
2021-09-25 20:33:10 +0100

    Added Train Search Function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Fragment } from 'react';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
+import Search from './components/TrainSearch/Search';
 
 const App = () => {
   return (
     <Fragment>
       <NavBar />
+      <Search />
 
     </Fragment>
   );
