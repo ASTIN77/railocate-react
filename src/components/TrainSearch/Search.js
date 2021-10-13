80a87e7a86dd6456c87e04af126193f53064f2fa
Andrew Tinsley <atinsley@astech-it.co.uk>
2021-10-13 17:52:01 +0100

    Reverted previous autocomplete logic

--- src/components/TrainSearch/Search.js
+++ src/components/TrainSearch/Search.js
@@ -1,5 +1,4 @@
 import { Fragment, useState } from "react";
-import useAutocomplete from "use-autocomplete";
 import searchData from "../../Data/railRef";
 import Results from "../TrainResults/Results";
 import axios from "axios";
@@ -9,10 +8,6 @@ import "../../Data/railRef";
 const Search = (props) => {
   const [enteredStationFrom, setEnteredStationFrom] = useState("");
   const [enteredStationTo, setEnteredStationTo] = useState("");
-  const [suggestedStationFrom] = useAutocomplete(
-    enteredStationFrom,
-    searchData
-  );
 
   const [trainResults, setTrainResults] = useState([]);
   const [isValid, setIsValid] = useState(false);
@@ -82,11 +77,6 @@ const Search = (props) => {
               onChange={fromStationInputHandler}
               value={enteredStationFrom}
             />
-            <div className="findStation form-control">
-              {suggestedStationFrom.map((val, index) => (
-                <p key={index}>{val}</p>
-              ))}
-            </div>
             <input
               type="text"
               className="findStation form-control"
